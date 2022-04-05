3d40e9c9c0a2f89fa8519ab080c96bcf973ee391
onlyphantom <samuel@outlook.sg>
2022-04-05 13:18:07 +0700

    post recording update

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import { useEffect, useState } from "react";
-
+import { useState, useEffect } from "react";
 import Web3 from "web3";
 import "./App.css";
 
 function App() {
+  const [account, setAccount] = useState();
+  const [network, setNetwork] = useState();
+  const [balance, setBalance] = useState();
 
-  const [account, setAccount] = useState(); // state variable to set account.
-  const [network, setNetwork] = useState('')
-  const [balance, setBalance] = useState(0)
+  // when app first mount, perform an action
+  const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+  useEffect(() => {
+    loadAccounts();
+  }, []);
 
   useEffect(() => {
-    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-    async function loadAccounts() {
-      const accounts = await web3.eth.requestAccounts();
-      // const network = await web3.eth.net.getNetworkType();
-      // const balance = await web3.eth.getBalance();
-      
-      // setNetwork(network);
-      // setBalance(balance);
-      setAccount(accounts[0]);
-    }
-    async function loadBalance() {
-      const network = await web3.eth.net.getNetworkType();
-      const balance = await web3.eth.getBalance(account)
-      
-      setNetwork(network);
-      // convert wei to ether
-      setBalance((balance/ 1e18).toFixed(4));
-    }
-
-    loadAccounts();
     loadBalance();
-   }, []);
+  }, [account]);
+
+  async function loadBalance() {
+    const network = await web3.eth.net.getNetworkType();
+    const balance = await web3.eth.getBalance(account, "latest");
+
+    setNetwork(network);
+    setBalance((balance / 1e18).toFixed(4));
+  }
+
+  async function loadAccounts() {
+    const accounts = await web3.eth.requestAccounts();
+    setAccount(accounts[0]);
+  }
 
   return (
     <div className="App">
-      <header className="App-header">Decentralized Ballot
-      <p>Your Account: {account}</p>
-      <p>Your Balance ({network}): {balance}</p>
+      <header className="App-header">
+        Decentralized Ballot
+        <p>Your Account: {account}</p>
+        <p>
+          Your Balance ({network}): {balance}
+        </p>
       </header>
     </div>
   );
